ba08a62252fe5e7de44f4be9c15c9223908de868
nobiscus <63877489+nobiscus@users.noreply.github.com>
2020-04-29 14:56:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,86 @@
+window.addEventListener("load", startGame);
+let point;
+let life;
+
+function startGame() {
+    console.log("startGame");
+    point = 0;
+    life = 3;
 
 
-// this is telling the browser to try to find an element with a class called ".mushroom" in the html file.
-document.querySelector(".mushroom");
+    /* See the game'screen, start playing the gaming sound,etc.
+    -----------------------------------------------------------
+    transitions: Click the edible -> clickEdible 1 2 4 5
+                 Click the inedible --> clickInedible 0 3 6 */
 
-// this is creating an empty box
-// the box is labeled "mushroom"
-// the box is constant, which means that if we put something in it, we cannot take it out or put anything else in it
-// constant = a quantity or parameter that does not change its value
-const mushroom;
+    document.querySelector("#mushroom_container1").addEventListener("click", clickEdible);
+    document.querySelector("#mushroom_container2").addEventListener("click", clickEdible);
+    document.querySelector("#mushroom_container4").addEventListener("click", clickEdible);
+    document.querySelector("#mushroom_container5").addEventListener("click", clickEdible);
+    document.querySelector("#mushroom_container0").addEventListener("click", clickInedible);
+    document.querySelector("#mushroom_container3").addEventListener("click", clickInedible);
+    document.querySelector("#mushroom_container6").addEventListener("click", clickInedible);
 
-// this will find an element with the class "mushroom" and put it into our box called mushroom
-// we are saving it in a box, so that we can manipulate it later
-const mushroom = document.querySelector(".mushroom");
-
-console.dir(mushroom);
-
-//
-document.querySelector(".mushroom").classList.add("falling");
-
-// console.log will show us the content of anything that we put into the parentheses
-//it logs the value (content) on the Console.
-// content = value
-//console.log();
-
-console.log("The content of the box is: ", mushroom);
-console.dir(mushroom);
-
-//const box = "car";
-
-
-//"let" is a variable, whih means that it can change content
-// the box is NOT constant, which means that if we put something in it, we CAN take it out or put anything else in it
-let secondBox = "car"; // let the content of secondBox be "car"
-secondBox = "red car";
-
-console.log(box);
-console.log(secondBox);
-
-let person = {
-  name: "Alexandra",
-  age: 33,
-  gender: "female",
-  clothes: ["tshirt", "skirt", "shoes", "hat"],
-  sayHello: function () {
-    console.log("Hello, I am Alexandra");
-  },
-};
-
-function sayHi() {
-  alert("hi");
 }
-// when Alexandra sees the event "adamjumps" she will perform the function "sayHi"
-//Alexandra.addEventListener("adamJumps", sayHi);
 
-window.addEventListener("click", sayHi);
-
-jump.addEventListener;
-
-console.log(boxForAperson);
-
-// if I write console.log("mushroom") it'll show it on the Console (in the browser) as such.
-//console.log("mushroom");
-// this is telling the browser to try to find an element with a class called ".mushroomCount" in the html file.
-const mushroomCounterSpan = document.querySelector(".mushroomCount");
-
-// let mushroomCount be zero
-let mushroomCount = 0;
-mushroom.addEventListener("click", () => {
-  console.log("You picked up a mushroom!");
-
-  mushroom.style.display = "none";
-  mushroom.classList.add("pickedUp");
-  mushroomCount = mushroomCount + 1;
-  mushroomCounterSpan.innerText = mushroomCount;
-});
+function clickEdible() {
+    console.log("clickEdible");
 
 
+    /* Mushroom disappears\ Earn 1 point \ See earned points
+    --------------------------------------------------------
+    When the disappearing animation is done --> newMushroom */
+    point++;
 
-//const coinContainer = document.querySelector("#coin_container1");
+    document.querySelector("#score").testContent = point;
+    document.querySelector("mushroom_container0").classList.add("disappear");
+    document.querySelector("mushroom_container1").classList.add("disappear");
+    document.querySelector("mushroom_container2").classList.add("disappear");
+    document.querySelector("mushroom_container3").classList.add("disappear");
+    document.querySelector("mushroom_container4").classList.add("disappear");
+    
+}
 
-//coinContainer.classList.add("falling");
-//coinContainer.classList.add("paused");
-//coinContainer.classList.add("falling");
+function newMushroom() {
+    console.log("newMushroom");
+    /* Remove the existing position \ relocate mushroom.
+    ---------------------------------------------------------
+    When all is done,the mushrooms reappear automatically + randomly, then see mushroom again. */
+}
 
-//
+function clickInedible() {
+    console.log("clickInedible");
+    /* Mushroom disappears \ lose 1 life \ see remaining hearts.
+    -----------------------------------------------------------
+    At no more hearts --> gameOver */
+
+}
+
+function stopGame() {
+    console.log("stopGame");
+    /* When all the transitions are over, remove all the eventListeners
+    --------------------------------------------------------------------------
+    For more than 10 accumulated points --> Level Complete
+    Or else --> gameOver */
+}
+
+function levelComplete() {
+    console.log("levelComplete");
+    // Write "Level Complete, congratulations!" in the console
+
+}
+
+function gameOver() {
+    console.log("gameOver");
+    // Write "Game Over" on the console
+}
+
+function blurryScreen() {
+    console.log("blurryScreen");
+
+}
+
+function timerOut() {
+    console.log("timerOut");
+
+}
